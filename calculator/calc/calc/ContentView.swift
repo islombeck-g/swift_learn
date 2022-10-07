ad367725b86a370ef49141f85a21c3d4bc0f6043
islombeck-g <whoisit864@gmail.com>
2022-10-07 17:16:00 +0300

    has errors

--- calculator/calc/calc/ContentView.swift
+++ calculator/calc/calc/ContentView.swift
@@ -17,8 +17,9 @@ struct ContentView: View {
         ["0","",".","="],
     ]
     let operators = ["/","+","%","*"]
-    @State var visibleWorkings=""
-    @State var visibleResults=""
+    @State var visibleWorkings   = ""
+    @State var visibleResults =  ""
+    @State var showAlert = false
     var body: some View {
         VStack
         {
@@ -42,22 +43,32 @@ struct ContentView: View {
             ForEach(grid, id:\.self)
             {
                 row in
-                HStack{
+                HStack
+                {
                     ForEach(row, id: \.self)
                     {
                         cell in
-                        Button { action:do {buttonPressed(cell: cell)}
-                        } label: {
+                        Button(action: { buttonPressed(cell: cell)}, label: {
                             Text(cell)
                                 .foregroundColor(buttonColor(cell))
                                 .font(.system(size: 40, weight: .heavy))
                                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        }
-                    }}
+                        })
+                    }
+                }
                 
             }
         }
         .background(Color.black.ignoresSafeArea())
+        .alert(isPresented:  $showAlert)
+        {
+            Alert(
+                title: Text("Invalid input"),
+                message:  Text(visibleWorkings),
+                dismissButton: .default(Text("Okay"))
+            )
+        }
+        
     }
     
     func buttonColor(_ cell:String)->Color
@@ -70,7 +81,8 @@ struct ContentView: View {
     }
     func buttonPressed(cell:String)
     {
-        switch cell {
+        switch cell
+        {
         case "AC":
             visibleWorkings=""
             visibleResults=""
@@ -78,23 +90,75 @@ struct ContentView: View {
             visibleWorkings=String(visibleWorkings.dropLast())
         case "=":
             visibleResults=calculateResult()
+        case "-":
+            addMinus()
+        case "/", "+", "X", "%":
+            addOperator(cell)
         default:
             visibleWorkings+=cell
         }
-        
-        
-        
     }
+    func addOperator(_ cell:String){
+        if !visibleWorkings.isEmpty
+        {
+            let last=String(visibleWorkings.last!)
+            if operators.contains(last) || last == "-"
+            {
+                visibleWorkings.removeLast()
+            }
+            visibleWorkings += cell
+        }
+    }
+    
+    func addMinus()
+    {
+        if visibleWorkings.isEmpty || visibleWorkings.last! != "-"
+        {
+            visibleWorkings += "-"
+        }
+    }
+    
+    
     func calculateResult()->String
     {
-        var workings=visibleWorkings.replacingOccurrences(of: <#T##StringProtocol#>, with: <#T##StringProtocol#>)
-        return ""
+        if(validInput())
+        {
+            var workings = visibleWorkings.replaceOccurrences(of: "%", with: "*0.01")
+            workings = visibleWorkings.replacingOccurrences(of: "X", with: "*")
+            let expression = NSExpression(format: workings)
+            let result = expression.expressionValue(withObject: nil, context: nil) as! Double
+            return formatResult(val:result)
+            
+        }
+        showAllert=true
+        return " "
+    }
+    func validinput()->Bool
+    {
+        if(visibleWorkings.isEmpty)
+        {
+            return false
+        }
+        let last = String(visibleWorking.last!)
+        if(operators.contains(last)||last=="-")
+        {
+            if(last  != "%" || visibleWorking.cout == 1)
+            {
+                return false
+            }
+        }
+        
+        return true
+    }
+    func formatResult(val: Double)->String
+    {
+        if(val.truncatingRemainder(dividingBy: 1)==0)
+        {
+            return String(format:"%.0f", val)
+        }
+        return String(format:"%.2f", val)
     }
 }
-
-
-
-
 
 
 
