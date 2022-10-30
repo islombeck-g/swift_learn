e6b37350c2a978d3a01bcebae693b78ed790da10
islombeck-g <whoisit864@gmail.com>
2022-10-30 13:42:59 +0300

    list and navigation
    done

--- swiftUiTutorial/swiftUiTutorial/Model/ModelData.swift
+++ swiftUiTutorial/swiftUiTutorial/Model/ModelData.swift
@@ -6,6 +6,13 @@
 //
 
 import Foundation
+import Combine
+
+final class ModelData: ObservableObject
+{
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
+
 
 var landmarks: [Landmark] = load("landmarkData.json")
 
--- swiftUiTutorial/swiftUiTutorial/Views/LandmarkList.swift
+++ swiftUiTutorial/swiftUiTutorial/Views/LandmarkList.swift
@@ -2,32 +2,40 @@
 import SwiftUI
 
 struct LandmarkList: View {
-   
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter {landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
-        NavigationView
-        {
-            List(landmarks){
-                landmark in
-                NavigationLink
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly)
                 {
-                    LandmarkDetail(landmark: landmark)
-                }label:{
-                    LandmarkRow(landmark: landmark)
+                    Text("Favorites only")
                 }
                 
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
             }
             .navigationTitle("Landmarks")
         }
-        
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-        }
-
+        LandmarkList()
+            .environmentObject(ModelData())
+        
     }
 }
