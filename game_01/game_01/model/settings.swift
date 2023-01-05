7261b0dc0fbe4c402f6c13b7a2952bbd7b87ee19
islombeck-g <whoisit864@gmail.com>
2023-01-05 04:54:18 +0300

    nightChanges)

--- game_01/game_01/model/settings.swift
+++ game_01/game_01/model/settings.swift
@@ -7,11 +7,117 @@
 
 import Foundation
 
-struct setings{
-    var countAllPlayers: Bool
-    var countOfMafia: Bool
-    var countOfDonMafia: Bool
-    var countOfManiac: Bool
-    var countOfDoctor: Bool
-    var countOfJournalist: Bool
+class GameSettings: ObservableObject {
+    @Published var countAllPlayers: Int
+    @Published var countOfMafia: Int
+    @Published var DonMafia: Bool
+    @Published var Maniac: Bool
+    @Published var Doctor: Bool
+    @Published var Journalist: Bool
+    var freePlaces: Int
+    
+    
+    init() {
+        self.countAllPlayers = 3
+        self.countOfMafia = 1
+        self.DonMafia = false
+        self.Maniac = false
+        self.Doctor = false
+        self.Journalist = false
+        self.freePlaces = 2
+    }
+    
+    func addCountAllPlayers(){
+        self.countAllPlayers = self.countAllPlayers + 1
+        self.freePlaces = self.freePlaces + 1
+    }
+    func minusCountAllPlayers(){
+        if self.countAllPlayers != 3{
+            self.countAllPlayers = self.countAllPlayers - 1
+            self.freePlaces = self.freePlaces - 1
+            if self.countAllPlayers/3 < self.countOfMafia{
+                minuscountOfMafia()
+            }
+        }
+    }
+    func printPeople() ->String{
+        return String(self.countAllPlayers)
+    }
+    func printMafia() ->String{
+        return String(self.countOfMafia)
+    }
+    func addCountOfMafia(){
+        if self.countAllPlayers/3 > self.countOfMafia{
+            self.countOfMafia = self.countOfMafia + 1
+            self.freePlaces = self.freePlaces - 1
+        }
+    }
+    func minuscountOfMafia(){
+        if self.countOfMafia != 1{
+            self.countOfMafia = Int(self.countOfMafia) - 1
+            self.freePlaces = self.freePlaces + 1
+        }
+    }
+    func donMafiaInGame(){
+        if self.DonMafia{
+            self.freePlaces = self.freePlaces + 1
+            self.DonMafia = false
+            
+        }
+        else{
+            if self.freePlaces > 0{
+                self.freePlaces = self.freePlaces - 1
+                self.DonMafia = true
+                
+            }
+            self.DonMafia = false
+            
+        }
+    }
+    func doctorInGame(){
+        if self.Doctor{
+            self.freePlaces = self.freePlaces + 1
+            self.Doctor = false
+            
+        }else{
+            if self.freePlaces > 0{
+                self.freePlaces = self.freePlaces - 1
+                self.Doctor = true
+                
+            }
+            self.Doctor = false
+            
+        }
+    }
+    func maniacInGame(){
+        if self.Maniac{
+            self.freePlaces = self.freePlaces + 1
+            self.Maniac = false
+            
+        }else{
+            if self.freePlaces > 0{
+                self.freePlaces = self.freePlaces - 1
+                self.Maniac = true
+                
+            }
+            self.Maniac = false
+            
+        }
+    }
+    func journalistInGame(){
+        if self.Journalist{
+            self.freePlaces = self.freePlaces + 1
+            self.Journalist = false
+            
+        }else{
+            if self.freePlaces > 0{
+                self.freePlaces = self.freePlaces - 1
+                self.Journalist = true
+                
+            }
+            self.Journalist = false
+            
+        }
+    }
 }
+
--- game_01/game_01/view/gameSettings.swift
+++ game_01/game_01/view/gameSettings.swift
@@ -1,49 +1,89 @@
-//
-//  gameSettings.swift
-//  game_01
-//
-//  Created by Islombek Gofurov on 03.01.2023.
-//
+
 
 import SwiftUI
 
 struct gameSettings: View {
     
-    @State private var doctor = false
-    @State private var donMafia = false
-    @State private var maniac = false
-    @State private var journalist = false
     
+    @StateObject private var s = GameSettings()
+    @State private var value = 0
+    @State private var step = 1
+    @State private var range = 0...50
     
     var body: some View {
-        NavigationView {
+        VStack{
+            //количество ролей
+            
             VStack{
-                //количество ролей
-                VStack{
-                    Text("Выберите количество ролей")
-                        .bold()
-                    HStack{
-                        Text("Всего игроков")
+                Text("Выберите количество ролей")
+                    .bold()
+                HStack{
+                    Text("Всего игроков")
+                    Button{
+                        s.minusCountAllPlayers()
+                    } label:{
+                        Image(systemName: "minus")
+                            .foregroundColor(Color("button_black_white"))
                     }
-                    HStack{
-                        Text("Мафия")
+                    Text(s.printPeople())
+                    Button{
+                        s.addCountAllPlayers()
+                    }label: {
+                        Image(systemName: "plus")
+                            .foregroundColor(Color("button_black_white"))
                     }
                 }
-                .padding(.top, 0.0)
-                Spacer()
-                //дополнительные роли
-                VStack{
-                    Text("Выберите дополнительные роли")
-                        .bold()
-                    
+                
+                
+                HStack{
+                    Text("Мафия")
                     Button{
-                        doctor.toggle()
+                        s.minuscountOfMafia()
+                    } label:{
+                        Image(systemName: "minus")
+                            .foregroundColor(Color("button_black_white"))
+                    }
+                    Text(s.printMafia())
+                    Button{
+                        s.addCountOfMafia()
                     }label: {
+                        Image(systemName: "plus")
+                            .foregroundColor(Color("button_black_white"))
+                    }
+                }
+            }
+            .padding(.top, 25.0)
+            Spacer()
+            //дополнительные роли
+            VStack{
+                Text("Выберите дополнительные роли")
+                    .bold()
+                
+                Button{
+                    s.donMafiaInGame()
+                }label: {
+                    if s.DonMafia{
                         HStack{
                             Image("hat")
                                 .resizable()
                                 .frame(width: 25.0, height: 25.0)
-                            
+                            Text("Дон мафии")
+                                .padding(.trailing, 29.0)
+                        }
+                        .frame(maxWidth: 270, minHeight: 40)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.black, lineWidth: 8))
+                        .background(Color.gray)
+                        .cornerRadius(10)
+                        .foregroundColor(Color("button_black_white"))
+                        
+                    }
+                    else{
+                        HStack{
+                            Image("hat")
+                                .resizable()
+                                .frame(width: 25.0, height: 25.0)
                             Text("Дон мафии")
                                 .padding(.trailing, 29.0)
                         }
@@ -51,10 +91,29 @@ struct gameSettings: View {
                         .background(Color.gray)
                         .cornerRadius(10)
                         .foregroundColor(Color("button_black_white"))
+                        
                     }
-                    Button{
-                        doctor.toggle()
-                    }label: {
+                }
+                
+                Button{
+                    s.maniacInGame()
+                }label: {
+                    if s.Maniac{
+                        HStack{
+                            Image("knife")
+                                .resizable()
+                                .frame(width: 25.0, height: 25.0)
+                            Text("Маньяк")
+                                .padding(.trailing, 29.0)
+                        }
+                        .frame(maxWidth: 270, minHeight: 40)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.black, lineWidth: 8))
+                        .background(Color.gray)
+                        .cornerRadius(10)
+                        .foregroundColor(Color("button_black_white"))
+                    }else{
                         HStack{
                             Image("knife")
                                 .resizable()
@@ -67,9 +126,28 @@ struct gameSettings: View {
                         .cornerRadius(10)
                         .foregroundColor(Color("button_black_white"))
                     }
-                    Button{
-                        doctor.toggle()
-                    }label: {
+                    
+                }
+                Button{
+                    s.doctorInGame()
+                }label: {
+                    if s.Doctor{
+                        HStack{
+                            Image("doctor")
+                                .resizable()
+                                .frame(width: 25.0, height: 25.0)
+                            Text("Доктор")
+                                .padding(.trailing, 29.0)
+                        }
+                        .frame(maxWidth: 270, minHeight: 40)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.black, lineWidth: 8))
+                        .background(Color.gray)
+                        .cornerRadius(10)
+                        .foregroundColor(Color("button_black_white"))
+                    }
+                    else{
                         HStack{
                             Image("doctor")
                                 .resizable()
@@ -81,47 +159,59 @@ struct gameSettings: View {
                         .background(Color.gray)
                         .cornerRadius(10)
                         .foregroundColor(Color("button_black_white"))
-                        
                     }
                     
-                    Button{
-                        doctor.toggle()
-                    }label: {
+                }
+                Button{
+                    s.journalistInGame()
+                }label: {
+                    if s.Journalist{
                         HStack{
                             Label("some", systemImage: "camera.fill")
                                 .padding(.trailing, 10.0)
                                 .labelStyle(IconOnlyLabelStyle())
-                            
                             Text("Журналист")
                                 .padding(.trailing, 29.0)
-                            
+                        }
+                        .frame(maxWidth: 270, minHeight: 40)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.black, lineWidth: 8))
+                        .background(Color.gray)
+                        .cornerRadius(10)
+                        .foregroundColor(Color("button_black_white"))
+                    }
+                    else{
+                        HStack{
+                            Label("some", systemImage: "camera.fill")
+                                .padding(.trailing, 10.0)
+                                .labelStyle(IconOnlyLabelStyle())
+                            Text("Журналист")
+                                .padding(.trailing, 29.0)
                         }
                         .frame(maxWidth: 270, minHeight: 40)
                         .background(Color.gray)
                         .cornerRadius(10)
                         .foregroundColor(Color("button_black_white"))
-                        
                     }
                     
                 }
-                
-                Spacer()
-                NavigationLink(destination: startGame())
-                {
-                    Text("создать игру")
-                }
-                .padding(.vertical, 12.0)
-                .padding(.horizontal, 45.0)
-                
-                .foregroundColor(Color("text_white_black"))
-                .background(
-                    Color("button_black_white")
-                        .cornerRadius(10))
             }
-        }
-        .navigationTitle("настройте игру")
-        
+            Spacer()
+            NavigationLink(destination: startGame())
+            {
+                Text("создать игру")
+            }
+            .padding(.vertical, 12.0)
+            .padding(.horizontal, 45.0)
+            .foregroundColor(Color("text_white_black"))
+            .background(
+                Color("button_black_white")
+                    .cornerRadius(10))
+        } .navigationTitle("настройте игру")
     }
+    
+    
 }
 
 
