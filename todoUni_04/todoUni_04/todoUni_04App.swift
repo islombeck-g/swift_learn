fa765c911af7bae04757940c77c9affe2b3a6192
islombeck-g <whoisit864@gmail.com>
2023-03-03 20:35:33 +0300

    some

---
+++ todoUni_04/todoUni_04/todoUni_04App.swift
@@ -0,0 +1,17 @@
+//
+//  todoUni_04App.swift
+//  todoUni_04
+//
+//  Created by Islombek Gofurov on 03.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct todoUni_04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
